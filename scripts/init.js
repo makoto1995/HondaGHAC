9aec9a38d702a8941798a222d2e9b39c7863648d
Gan Kai Hong <kevin_dante@foxmail.com>
2018-12-20 21:36:55 +0800

    Rearranged Functions

--- scripts/init.js
+++ scripts/init.js
@@ -31,3 +31,54 @@ function getProcessData( prev, range) {
     return rnd;
 }
 
+var horizontalReportData = {
+	'totalConsumption':1167677,
+	'PlantA': 184511,
+	'buswayLV': 539928,
+	'equipUPS': 1470,
+	'genUPS': 1867,
+	'PlantB': 38323,
+	'DataCenter': 30333,
+	'ConnectedLoads': 4563,
+	'chillerUnits': 197177,
+	'chillerCP': 414,
+	'MAU': 36456,
+	'PCW': 32088,
+	'CWP': 14736,
+	'CWSP': 7752,
+	'CTF': 12960,
+	'CTMF': 150,
+	'CWPP': 11352,
+	'RCU': 3776,
+	'AHU': 3936,
+	'FFU': 9344,
+	'scrubberExhaust': 16608,
+	'SRP': 1528,
+	'generalExhaust': 2643,
+	'solventExhaust': 1384,
+	'WWTP': 456,
+	'PWP': 539,
+	'TCM': 600,
+	'TGM': 466,
+	'PVP': 3365,
+	'lighting': 9035,
+	'boiler': 743,
+	'DWP': 151,
+	'CCTV': 221,
+	'scada': 566
+	
+};
+
+for (var i in horizontalReportData) {
+    var data = horizontalReportData[i] + (horizontalReportData[i]*0.2*Math.random());
+	setTag( i , data);
+}
+
+var dummylist = {
+	'dummyText': 'KWH Per Day'
+};
+
+for (var i in dummylist) {
+	setTag( i , dummylist[i] );
+}
+
